2fae6a5f15ee23bed1809a98c0a72c2f9bc0ebc0
Greg Stimson <stimsong@yahoo.com>
2018-02-05 21:29:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   turn_count.each |occupied| do
-    if (occupied == "X" or occupied == "O")
+    if(occupied == "X" or occupied == "O")
       counter += 1
     end
   end
